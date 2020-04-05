1f4cd36ef7497adf48710f015a4af6af3fbad9a0
Anton Dementev <antdementev@gmail.com>
2020-04-06 00:12:54 +0300

    add lombok and logger

--- src/main/java/ru/dementev/controller/Controller.java
+++ src/main/java/ru/dementev/controller/Controller.java
@@ -1,16 +1,19 @@
 package ru.dementev.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class Controller {
     @Value("${hello.world:Hello World}")
     private String hello;
 
     @GetMapping("/testing")
     public String testing(){
+        log.info("Response " + hello);
         return hello;
     }
 
