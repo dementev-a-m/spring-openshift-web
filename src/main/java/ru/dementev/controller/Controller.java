869b9719ca57e5d8a4b2486a86eb925a98dd489d
Anton Dementev <antdementev@gmail.com>
2020-04-06 12:24:25 +0300

    fix application.yml

--- src/main/java/ru/dementev/controller/Controller.java
+++ src/main/java/ru/dementev/controller/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class Controller {
-    @Value("${hello.world:Hello World}")
+    @Value("${soring.web.response:Hello World}")
     private String hello;
 
     @GetMapping("/testing")
